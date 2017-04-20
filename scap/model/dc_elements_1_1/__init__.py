145a0d30306a7af255c6f6e7dd48639eebc70d0f
Casey Jaymes <cjaymes@gmail.com>
2017-04-20 11:25:51 -0400

    stubbed dc elements

---
+++ scap/model/dc_elements_1_1/__init__.py
@@ -0,0 +1,33 @@
+# Copyright 2016 Casey Jaymes
+
+# This file is part of PySCAP.
+#
+# PySCAP is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PySCAP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PySCAP.  If not, see <http://www.gnu.org/licenses/>.
+
+TAG_MAP = {
+    '{http://purl.org/dc/elements/1.1/}title': {'class': 'Title'},
+    '{http://purl.org/dc/elements/1.1/}creator': {'class': 'Creator'},
+    '{http://purl.org/dc/elements/1.1/}subject': {'class': 'Subject'},
+    '{http://purl.org/dc/elements/1.1/}description': {'class': 'Description'},
+    '{http://purl.org/dc/elements/1.1/}publisher': {'class': 'Publisher'},
+    '{http://purl.org/dc/elements/1.1/}contributor': {'class': 'Contributor'},
+    '{http://purl.org/dc/elements/1.1/}date': {'class': 'Date'},
+    '{http://purl.org/dc/elements/1.1/}type': {'class': 'Type'},
+    '{http://purl.org/dc/elements/1.1/}format': {'class': 'Format'},
+    '{http://purl.org/dc/elements/1.1/}identifier': {'class': 'Identifier'},
+    '{http://purl.org/dc/elements/1.1/}language': {'class': 'Language'},
+    '{http://purl.org/dc/elements/1.1/}relation': {'class': 'Relation'},
+    '{http://purl.org/dc/elements/1.1/}coverage': {'class': 'Coverage'},
+    '{http://purl.org/dc/elements/1.1/}rights': {'class': 'Rights'},
+}
