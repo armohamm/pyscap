748026d280c6de5ccb286043a4adf44389696cda
Casey Jaymes <cjaymes@gmail.com>
2017-05-08 15:46:25 -0400

    added windows id detection

---
+++ scap/collector/cli/windows/IdentityCollector.py
@@ -0,0 +1,46 @@
+# Copyright 2016 Casey Jaymes
+
+# This file is part of PySCAP.
+#
+# PySCAP is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PySCAP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PySCAP.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+
+from scap.collector.cli.WindowsCollector import WindowsCollector
+
+logger = logging.getLogger(__name__)
+class IdentityCollector(WindowsCollector):
+    def collect(self):
+        if 'identity' in self.host.facts:
+            return
+
+        self.host.facts['identity']['user_name'] = self.host.exec_command('echo %USERNAME%')
+        try:
+            self.host.facts['identity']['user_dns_domain'] = self.host.exec_command('echo %USERDNSDOMAIN%')
+        except:
+            pass
+        try:
+            self.host.facts['identity']['user_domain'] = self.host.exec_command('echo %USERDOMAIN%')
+        except:
+            pass
+        self.host.facts['identity']['computer_name'] = self.host.exec_command('echo %COMPUTERNAME%')
+
+        self.host.facts['identity']['authenticated'] = True
+        self.host.facts['identity']['name'] = self.host.facts['identity']['user_name']
+
+        try:
+            self.host.exec_command('su', elevate=True)
+            self.host.facts['identity']['privileged'] = True
+        except ElevationException:
+            self.host.facts['identity']['privileged'] = False
