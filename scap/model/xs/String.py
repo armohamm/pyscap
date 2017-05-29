7c416a1b09d2e1018f3cad056f29194a30f068dc
Casey Jaymes <cjaymes@gmail.com>
2017-05-29 15:27:09 -0400

    added enum matching

--- scap/model/xs/String.py
+++ scap/model/xs/String.py
@@ -25,12 +25,19 @@ class String(AnySimpleType):
     def get_value_pattern(self):
         return None
 
+    def get_value_enum(self):
+        return None
+
     def parse_value(self, value):
         if not isinstance(value, str):
-            raise TypeError('xs:string requires a str for initialization, got ' + value.__class__.__name__)
+            raise TypeError('xs:string requires a str value for initialization, got ' + value.__class__.__name__)
 
         p = self.get_value_pattern()
         if p is not None and not re.fullmatch(p, value):
-            raise ValueError(self.__class__.__name__ + ' requires a str matching ' + p)
+            raise ValueError(self.__class__.__name__ + ' requires a value matching ' + p)
+
+        e = self.get_value_pattern()
+        if e is not None and value not in e:
+            raise ValueError(self.__class__.__name__ + ' requires a value in ' + str(e))
 
         return value
