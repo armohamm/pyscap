1a2627886058007ccccaf76e0ea3a33ce660c32d
Casey Jaymes <cjaymes@gmail.com>
2017-06-03 23:17:31 -0400

    added aliases

--- scap/model/oval/oval_5_3/defs/OvalDefinitionsElement.py
+++ scap/model/oval/oval_5_3/defs/OvalDefinitionsElement.py
@@ -27,11 +27,26 @@ class OvalDefinitionsElement(Model):
         'tag_name' : 'oval_definitions',
         'elements': [
             {'xmlns': 'http://oval.mitre.org/XMLSchema/oval-common-5', 'tag_name': 'generator', 'class': 'GeneratorType', 'min': 0},
-            {'tag_name': 'definitions', 'class': 'DefinitionsType', 'min': 0, 'max': 1},
-            {'tag_name': 'tests', 'class': 'TestsType', 'min': 0, 'max': 1},
-            {'tag_name': 'objects', 'class': 'ObjectsType', 'min': 0, 'max': 1},
-            {'tag_name': 'states', 'class': 'StatesType', 'min': 0, 'max': 1},
-            {'tag_name': 'variables', 'class': 'VariablesType', 'min': 0, 'max': 1},
+            {'tag_name': '_definitions', 'class': 'DefinitionsType', 'min': 0, 'max': 1},
+            {'tag_name': '_tests', 'class': 'TestsType', 'min': 0, 'max': 1},
+            {'tag_name': '_objects', 'class': 'ObjectsType', 'min': 0, 'max': 1},
+            {'tag_name': '_states', 'class': 'StatesType', 'min': 0, 'max': 1},
+            {'tag_name': '_variables', 'class': 'VariablesType', 'min': 0, 'max': 1},
             {'xmlns': 'http://www.w3.org/2000/09/xmldsig#', 'tag_name': 'Signature', 'min': 0, 'max': 1},
         ],
     }
+
+    def from_xml(self, parent, el):
+        super(OvalDefinitionsElement, self).from_xml(parent, el)
+
+        # set some convenient alias attributes
+        if self._definitions is not None:
+            self.definitions = self._definitions.definitions
+        if self._tests is not None:
+            self.tests = self._tests.tests
+        if self._objects is not None:
+            self.objects = self._objects.objects
+        if self._states is not None:
+            self.states = self._states.states
+        if self._variables is not None:
+            self.variables = self._variables.variables
