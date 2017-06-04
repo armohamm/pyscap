5d0e57d55dfa468702eadbe854ab237ef8691f41
Casey Jaymes <cjaymes@gmail.com>
2017-06-04 00:34:03 -0400

    added oval_5_3 linux defs

---
+++ scap/model/oval/oval_5_3/defs/linux/__init__.py
@@ -0,0 +1,33 @@
+# Copyright 2016 Casey Jaymes
+
+# This file is part of PySCAP.
+#
+# PySCAP is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PySCAP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PySCAP.  If not, see <http://www.gnu.org/licenses/>.
+TAG_MAP = {
+    '{http://oval.mitre.org/XMLSchema/oval-definitions-5#linux}dpkginfo_test': 'DpkgInfoTestElement',
+    '{http://oval.mitre.org/XMLSchema/oval-definitions-5#linux}dpkginfo_object': 'DpkgInfoObjectElement',
+    '{http://oval.mitre.org/XMLSchema/oval-definitions-5#linux}dpkginfo_state': 'DpkgInfoStateElement',
+
+    '{http://oval.mitre.org/XMLSchema/oval-definitions-5#linux}inetlisteningservers_test': 'InetListeningServersTestElement',
+    '{http://oval.mitre.org/XMLSchema/oval-definitions-5#linux}inetlisteningservers_object': 'InetListeningServersObjectElement',
+    '{http://oval.mitre.org/XMLSchema/oval-definitions-5#linux}inetlisteningservers_state': 'InetListeningServersStateElement',
+
+    '{http://oval.mitre.org/XMLSchema/oval-definitions-5#linux}rpminfo_test': 'RpmInfoTestElement',
+    '{http://oval.mitre.org/XMLSchema/oval-definitions-5#linux}rpminfo_object': 'RpmInfoObjectElement',
+    '{http://oval.mitre.org/XMLSchema/oval-definitions-5#linux}rpminfo_state': 'RpmInfoStateElement',
+
+    '{http://oval.mitre.org/XMLSchema/oval-definitions-5#linux}slackwarepkginfo_test': 'SlackwarePkgInfoTestElement',
+    '{http://oval.mitre.org/XMLSchema/oval-definitions-5#linux}slackwarepkginfo_object': 'SlackwarePkgInfoObjectElement',
+    '{http://oval.mitre.org/XMLSchema/oval-definitions-5#linux}slackwarepkginfo_state': 'SlackwarePkgInfoStateElement',
+}
