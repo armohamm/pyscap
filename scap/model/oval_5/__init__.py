bd423c8083dccaddfda28f630941e0b0051e0293
Casey Jaymes <cjaymes@gmail.com>
2017-06-07 23:39:08 -0400

    made functions more efficient

--- scap/model/oval_5/__init__.py
+++ scap/model/oval_5/__init__.py
@@ -29,7 +29,7 @@ CHECK_ENUMERATION = [
     'only one',
 ]
 
-CHECK_RESULT_ENUMERATION = [
+RESULT_ENUMERATION = [
     'true',
     'false',
     'error',
@@ -38,78 +38,62 @@ CHECK_RESULT_ENUMERATION = [
     'not applicable',
 ]
 
-def count_check_results(results):
-    counts = {}
-    for i in CHECK_RESULT_ENUMERATION:
-        counts[i] = 0
-    for r in results:
-        counts[r] += 1
-    return counts['true'], counts['false'], counts['error'], counts['unknown'], counts['not evaluated'], counts['not applicable']
-
-def check_all(results):
-    true, false, error, unknown, not_evaluated, not_applicable = count_check_results(results)
-
-    if true >= 1 and false == 0 and error == 0 and unknown == 0 and not_evaluated == 0:
+def check_all(t, f, e, u, ne, na):
+    if t >= 1 and f == 0 and e == 0 and u == 0 and ne == 0:
         return 'true'
-    elif false >= 1:
+    elif f >= 1:
         return 'false'
-    elif false == 0 and error >= 1:
+    elif f == 0 and e >= 1:
         return 'error'
-    elif false == 0 and error == 0 and unknown >= 1:
+    elif f == 0 and e == 0 and u >= 1:
         return 'unknown'
-    elif false == 0 and error == 0 and unknown == 0 and not_evaluated >= 1:
+    elif f == 0 and e == 0 and u == 0 and ne >= 1:
         return 'not evaluated'
-    elif true == 0 and false == 0 and error == 0 and unknown == 0 and not_evaluated == 0 and not_applicable >= 1:
+    elif t == 0 and f == 0 and e == 0 and u == 0 and ne == 0 and na >= 1:
         return 'not applicable'
 
-def check_at_least_one(results):
-    true, false, error, unknown, not_evaluated, not_applicable = count_check_results(results)
-
-    if true >= 1:
+def check_at_least_one(t, f, e, u, ne, na):
+    if t >= 1:
         return 'true'
-    elif true == 0 and false >= 1 and error == 0 and unknown == 0 and not_evaluated == 0:
+    elif t == 0 and f >= 1 and e == 0 and u == 0 and ne == 0:
         return 'false'
-    elif true == 0 and error >= 1:
+    elif t == 0 and e >= 1:
         return 'error'
-    elif true == 0 and error == 0 and unknown >= 1:
+    elif t == 0 and e == 0 and u >= 1:
         return 'unknown'
-    elif true == 0 and error == 0 and unknown == 0 and not_evaluated >= 1:
+    elif t == 0 and e == 0 and u == 0 and ne >= 1:
         return 'not evaluated'
-    elif true == 0 and false == 0 and error == 0 and unknown == 0 and not_evaluated == 0 and not_applicable >= 1:
+    elif t == 0 and f == 0 and e == 0 and u == 0 and ne == 0 and na >= 1:
         return 'not applicable'
 
-def check_none_satisfy(results):
-    true, false, error, unknown, not_evaluated, not_applicable = count_check_results(results)
-
-    if true == 0 and false >= 1 and error == 0 and unknown == 0 and not_evaluated == 0:
+def check_none_satisfy(t, f, e, u, ne, na):
+    if t == 0 and f >= 1 and e == 0 and u == 0 and ne == 0:
         return 'true'
-    elif true >= 1:
+    elif t >= 1:
         return 'false'
-    elif true == 0 and error >= 1:
+    elif t == 0 and e >= 1:
         return 'error'
-    elif true == 0 and error == 0 and unknown >= 1:
+    elif t == 0 and e == 0 and u >= 1:
         return 'unknown'
-    elif true == 0 and error == 0 and unknown == 0 and not_evaluated >= 1:
+    elif t == 0 and e == 0 and u == 0 and ne >= 1:
         return 'not evaluated'
-    elif true == 0 and false == 0 and error == 0 and unknown == 0 and not_evaluated == 0 and not_applicable >= 1:
+    elif t == 0 and f == 0 and e == 0 and u == 0 and ne == 0 and na >= 1:
         return 'not applicable'
 
-def check_only_one(results):
-    true, false, error, unknown, not_evaluated, not_applicable = count_check_results(results)
-
-    if true == 1 and error == 0 and unknown == 0 and not_evaluated == 0:
+def check_only_one(t, f, e, u, ne, na):
+    if t == 1 and e == 0 and u == 0 and ne == 0:
         return 'true'
-    elif true >= 1:
+    elif t >= 1:
         return 'false'
-    elif true == 0 and false >= 1 and error == 0 and unknown == 0 and not_evaluated == 0:
+    elif t == 0 and f >= 1 and e == 0 and u == 0 and ne == 0:
         return 'false'
-    elif (true == 0 or true == 1) and error >=1:
+    elif (t == 0 or t == 1) and e >=1:
         return 'error'
-    elif (true == 0 or true == 1) and error == 0 and unknown >= 1:
+    elif (t == 0 or t == 1) and e == 0 and u >= 1:
         return 'unknown'
-    elif (true == 0 or true == 1) and error == 0 and unknown == 0 and not_evaluated >= 1:
+    elif (t == 0 or t == 1) and e == 0 and u == 0 and ne >= 1:
         return 'not evaluated'
-    elif true == 0 and false == 0 and error == 0 and unknown == 0 and not_evaluated == 0 and not_applicable >= 1:
+    elif t == 0 and f == 0 and e == 0 and u == 0 and ne == 0 and na >= 1:
         return 'not applicable'
 
 CLASS_ENUMERATION = [
@@ -156,76 +140,56 @@ EXISTENCE_RESULT_ENUMERATION = [
     'not collected',
 ]
 
-def count_existence_results(item_status_values):
-    counts = {}
-    for i in EXISTENCE_RESULT_ENUMERATION:
-        counts[i] = 0
-
-    for v in item_status_values:
-        counts[v] += 1
-
-    return counts['exists'], counts['does not exist'], counts['error'], counts['not collected']
-
-def existence_all_exist(item_status_values):
-    exists, does_not_exist, error, not_collected = count_existence_results(item_status_values)
-
-    if exists >= 1 and does_not_exist == 0 and error == 0 and not_collected == 0:
+def existence_all_exist(ex, dne, e, nc):
+    if ex >= 1 and dne == 0 and e == 0 and nc == 0:
         return 'true'
-    elif exists == 0 and does_not_exist == 0 and error == 0 and not_collected == 0:
+    elif ex == 0 and dne == 0 and e == 0 and nc == 0:
         return 'false'
-    elif does_not_exist >= 1:
+    elif dne >= 1:
         return 'false'
-    elif does_not_exist == 0 and error >= 1:
+    elif dne == 0 and e >= 1:
         return 'error'
-    elif does_not_exist == 0 and error == 0 and not_collected >= 1:
+    elif dne == 0 and e == 0 and nc >= 1:
         return 'unknown'
 
-def existence_any_exist(item_status_values):
-    exists, does_not_exist, error, not_collected = count_existence_results(item_status_values)
-
-    if error == 0:
+def existence_any_exist(ex, dne, e, nc):
+    if e == 0:
         return 'true'
-    elif exists >= 1 and error >= 1:
+    elif ex >= 1 and e >= 1:
         return 'true'
-    elif exists == 0 and error >= 1:
+    elif ex == 0 and e >= 1:
         return 'error'
 
-def existence_at_least_one_exists(item_status_values):
-    exists, does_not_exist, error, not_collected = count_existence_results(item_status_values)
-
-    if exists >= 1:
+def existence_at_least_one_exists(ex, dne, e, nc):
+    if ex >= 1:
         return 'true'
-    elif exists == 0 and does_not_exist >= 1 and error == 0 and not_collected == 0:
+    elif ex == 0 and dne >= 1 and e == 0 and nc == 0:
         return 'false'
-    elif exists == 0 and error >= 1:
+    elif ex == 0 and e >= 1:
         return 'error'
-    elif exist == 0 and error == 0 and not_collected >= 1:
+    elif exist == 0 and e == 0 and nc >= 1:
         return 'unknown'
 
-def existence_none_exist(item_status_values):
-    exists, does_not_exist, error, not_collected = count_existence_results(item_status_values)
-
-    if exists == 0 and error == 0 and not_collected == 0:
+def existence_none_exist(ex, dne, e, nc):
+    if ex == 0 and e == 0 and nc == 0:
         return 'true'
-    elif exists >= 1:
+    elif ex >= 1:
         return 'false'
-    elif exists == 0 and error >= 1:
+    elif ex == 0 and e >= 1:
         return 'error'
-    elif exists == 0 and error == 0 and not_collected >= 1:
+    elif ex == 0 and e == 0 and nc >= 1:
         return 'unknown'
 
-def existence_only_one_exists(item_status_values):
-    exists, does_not_exist, error, not_collected = count_existence_results(item_status_values)
-
-    if exists == 1 and error == 0 and not_collected == 0:
+def existence_only_one_exists(ex, dne, e, nc):
+    if ex == 1 and e == 0 and nc == 0:
         return 'true'
-    elif exists >= 2:
+    elif ex >= 2:
         return 'false'
-    elif exists == 0 and error == 0 and not_collected == 0:
+    elif ex == 0 and e == 0 and nc == 0:
         return 'false'
-    elif (exists == 0 or exists == 1) and error >= 1:
+    elif (ex == 0 or ex == 1) and e >= 1:
         return 'error'
-    elif (exists == 0 or exists == 1) and error == 0 and not_collected >= 1:
+    elif (ex == 0 or ex == 1) and e == 0 and nc >= 1:
         return 'unknown'
 
 FAMILY_ENUMERATION = [
@@ -275,30 +239,6 @@ OPERATOR_ENUMERATION = [
     'XOR',
 ]
 
-# NOTE: we include negate here as a convenient place to store the function
-def operator_negate(value):
-    if value == 'true':
-        return 'false'
-    elif value == 'false':
-        return 'true'
-    else:
-        return value
-
-def count_operator_results(values):
-    counts = {
-        'true': 0,
-        'false': 0,
-        'error': 0,
-        'unknown': 0,
-        'not evaluated': 0,
-        'not applicable': 0,
-    }
-    for val in values:
-        counts[val] += 1
-
-    return counts['true'], counts['false'], counts['error'], counts['unknown'], \
-        counts['not evaluated'], counts['not applicable']
-
 # ---------------||-----------------------------||------------------
 #                ||  num of individual results  ||
 #   operator is  ||                             ||  final result is
@@ -311,8 +251,7 @@ def count_operator_results(values):
 #                || 0+ | 0  | 0  | 0  | 1+ | 0+ ||  Not Evaluated
 #                || 0  | 0  | 0  | 0  | 0  | 1+ ||  Not Applicable
 # ---------------||-----------------------------||------------------
-def operator_AND(values):
-    t, f, e, u, ne, na = count_operator_results(values)
+def operator_AND(t, f, e, u, ne, na):
     if t >= 1 and f == 0 and e == 0 and u == 0 and ne == 0:
         return 'true'
     elif f >= 1:
@@ -339,8 +278,7 @@ def operator_AND(values):
 #                ||0,1 | 0+ | 0  | 0  | 1+ | 0+ ||  Not Evaluated
 #                || 0  | 0  | 0  | 0  | 0  | 1+ ||  Not Applicable
 # ---------------||-----------------------------||------------------
-def operator_ONE(values):
-    t, f, e, u, ne, na = count_operator_results(values)
+def operator_ONE(t, f, e, u, ne, na):
     if t == 1 and e == 0 and u == 0 and ne == 0:
         return 'true'
     elif t >= 2:
@@ -368,8 +306,7 @@ def operator_ONE(values):
 #                || 0  | 0+ | 0  | 0  | 1+ | 0+ ||  Not Evaluated
 #                || 0  | 0  | 0  | 0  | 0  | 1+ ||  Not Applicable
 # ---------------||-----------------------------||------------------
-def operator_OR(values):
-    t, f, e, u, ne, na = count_operator_results(values)
+def operator_OR(t, f, e, u, ne, na):
     if t >= 1:
         return 'true'
     elif t == 0 and f >= 1 and e == 0 and u == 0 and ne == 0:
@@ -395,8 +332,7 @@ def operator_OR(values):
 #                || 0+ | 0+ | 0  | 0  | 1+ | 0+ ||  Not Evaluated
 #                || 0  | 0  | 0  | 0  | 0  | 1+ ||  Not Applicable
 # ---------------||-----------------------------||------------------
-def operator_XOR(values):
-    t, f, e, u, ne, na = count_operator_results(values)
+def operator_XOR(t, f, e, u, ne, na):
     if t % 2 == 1 and e == 0 and u == 0 and ne == 0:
         return 'true'
     elif t % 2 == 0 and e == 0 and u == 0 and ne == 0:
