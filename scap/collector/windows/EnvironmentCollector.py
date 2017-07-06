9b8e9f21feeea2492cb5e71610e3e86332f08790
Casey Jaymes <cjaymes@gmail.com>
2017-07-06 11:12:00 -0400

    implemented windows #independent collectors

---
+++ scap/collector/windows/EnvironmentCollector.py
@@ -0,0 +1,41 @@
+# Copyright 2016 Casey Jaymes
+
+# This file is part of PySCAP.
+#
+# PySCAP is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PySCAP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PySCAP.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+import json
+import pprint
+import re
+
+from scap.Collector import Collector, ArgumentException
+
+logger = logging.getLogger(__name__)
+class EnvironmentCollector(Collector):
+    def collect(self):
+        cmd = 'Get-ChildItem Env:'
+        cmd = cmd + " | Select-Object -Property * | ConvertTo-Json -Compress"
+        return_code, out_lines, err_lines = self.host.exec_command('powershell -Command \"' + cmd + '\"')
+
+        s = ''
+        for l in out_lines:
+            s = s + l
+        j = json.loads(s)
+
+        env = {}
+        for e in j:
+            env[e['Key']] = e['Value']
+
+        return env
---
+++ scap/collector/windows/EnvironmentVariableCollector.py
@@ -0,0 +1,47 @@
+# Copyright 2016 Casey Jaymes
+
+# This file is part of PySCAP.
+#
+# PySCAP is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PySCAP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PySCAP.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+import json
+
+from scap.Collector import Collector, ArgumentException
+
+logger = logging.getLogger(__name__)
+class EnvironmentVariableCollector(Collector):
+    def __init__(self, host, args):
+        super(EnvironmentVariableCollector, self).__init__(host, args)
+
+        if 'name' not in args:
+            raise ArgumentException('EnvironmentVariableCollector requires name argument')
+
+    def collect(self):
+        cmd = 'Get-ChildItem Env:' + self.args['name']
+        cmd = cmd + " | Select-Object -Property * | ConvertTo-Json -Compress"
+        return_code, out_lines, err_lines = self.host.exec_command('powershell -Command \"' + cmd + '\"')
+        if return_code != 0 or len(out_lines) < 1 or out_lines[0] == '':
+            raise KeyError('Environment variable ' + self.args['name'] + ' was not found')
+
+        s = ''
+        for l in out_lines:
+            s = s + l
+        j = json.loads(s)
+
+        #logger.debug(str(j))
+        if 'Name' not in j or j['Name'] != self.args['name']:
+            raise KeyError('Environment variable ' + self.args['name'] + ' was not found')
+
+        return j['Value']
