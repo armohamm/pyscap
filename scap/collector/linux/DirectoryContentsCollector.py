af86cd6a33daa60948f476a0bde728da8422fca7
Casey Jaymes <cjaymes@gmail.com>
2017-07-07 17:34:25 -0400

    fixed dir collector datetime parsing

--- scap/collector/linux/DirectoryContentsCollector.py
+++ scap/collector/linux/DirectoryContentsCollector.py
@@ -57,12 +57,26 @@ class DirectoryContentsCollector(Collector):
 
         entries = []
         for l in out_lines:
-            if re.fullmatch(r'\w+\s+[0-9]+'):   # total line
+            if re.fullmatch(r'\w+\s+[0-9]+', l):   # total line
                 continue
 
             #2011-11-08 18:02:08.954092000 -0700
             m = re.fullmatch(r'([-a-z])([-sStTrwx]{3})([-sStTrwx]{3})([-rwx]{3})(\.)?\s+([0-9]+)\s+(\S+)\s+(\S+)\s+([0-9]+)\s+([0-9]+-[0-9]+-[0-9]+\s+[0-9]+:[0-9]+:[0-9.]+\s+[0-9-]+)\s+(.*)( -> (.*))?', l)
             if m:
+                mtime = m.group(10)
+                logger.debug('original mtime: ' + str(mtime))
+
+                # an unfortunate circumstance of the mtime %f not always being 6 or less chars
+                mtime = m.group(10).partition('.')
+                msecs = mtime[2]
+                mtime = mtime[0] + '.'
+                msecs = msecs.partition(' ')
+                mtime = mtime + msecs[0][:5] + ' ' + msecs[2]
+                logger.debug('reduced precision mtime: ' + str(mtime))
+
+                mtime = datetime.datetime.strptime(mtime, '%Y-%m-%d %H:%M:%S.%f %z')
+                logger.debug('parsed mtime: ' + str(mtime))
+
                 entry = {
                     'type': DirectoryContentsCollector.TYPE_MAP[m.group(1)],
                     'user_mode': m.group(2),
@@ -73,7 +87,7 @@ class DirectoryContentsCollector(Collector):
                     'group_owner': m.group(8),
                     'size': int(m.group(9)),
                     'modified_raw': m.group(10),
-                    'modified': datetime.strptime(m.group(10), '%Y-%m-%d %H:%M:%S.%f %z'),
+                    'modified': mtime,
                     'name': m.group(11),
                 }
                 if m.group(5) is not None:
