afb792124b5b48a79bdf78ba578580ff4ee9b566
Casey Jaymes <cjaymes@gmail.com>
2017-08-07 20:52:22 -0400

    added vmdetec prequisite

--- test/collector/linux/test_blkid.py
+++ test/collector/linux/test_blkid.py
@@ -37,6 +37,7 @@ except IOError:
 host = Host.load('localhost')
 
 def setup_module(module):
+    host.load_collector('VirtualMachineDetectionCollector', {}).collect()
     if host.facts['oval_family'] != 'linux':
         pytest.skip('Does not apply to platform')
     if host.facts['in_virtual_machine'] and host.facts['hosting_hypervisor'] == 'docker':
