ce975b5287492f813b733cb203aa699bd6e967df
Casey Jaymes <cjaymes@gmail.com>
2017-08-07 20:30:36 -0400

    added blkid, findmnt collectors for root fs uuid & tests

--- scap/collector/linux/RootFsUuidCollector.py
+++ scap/collector/linux/RootFsUuidCollector.py
@@ -22,6 +22,19 @@ from scap.Collector import Collector
 logger = logging.getLogger(__name__)
 class RootFsUuidCollector(Collector):
     def collect(self):
-        return_code, out_lines, err_lines = self.host.exec_command("blkid -o value `mount -l | grep 'on / ' | awk '{print $1}'` | head -n1", sudo=True)
-        self.host.facts['root_uuid'] = out_lines[0]
-        logger.debug('Root FS UUID: ' + self.host.facts['root_uuid'])
+        # return_code, out_lines, err_lines = self.host.exec_command("blkid -o value -s UUID `mount -l | grep 'on / ' | awk '{print $1}'`")
+        # if return_code != 0 or len(out_lines) <= 0:
+        #     raise RuntimeError('Unable to determine unique id from root fs uuid')
+        #
+        # self.host.facts['root_filesystem_uuid'] = out_lines[0].strip()
+        from .BlkidCollector import BlkidCollector
+        BlkidCollector(self.host, {}).collect()
+        from .FindMntCollector import FindMntCollector
+        FindMntCollector(self.host, {}).collect()
+        try:
+            mountpoint = self.host.facts['mountpoints']['/']['source']
+            self.host.facts['root_filesystem_uuid'] = self.host.facts['blkid'][mountpoint]['uuid']
+        except KeyError:
+            raise RuntimeError('Unable to determine unique id from root fs uuid')
+
+        logger.debug('Root FS UUID: ' + self.host.facts['root_filesystem_uuid'])
