a6a6ecb703e2cdbf56fd0cbbee994ff008dc76c4
Casey Jaymes <cjaymes@gmail.com>
2017-08-19 17:45:51 -0400

    fixed imports

--- src/scap/model/ocil_2_0/ArtifactIDPattern.py
+++ src/scap/model/ocil_2_0/ArtifactIDPattern.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with PySCAP.  If not, see <http://www.gnu.org/licenses/>.
 
+import logging
+
 from scap.model.decorators import *
 from scap.model.xs.TokenType import TokenType
 
