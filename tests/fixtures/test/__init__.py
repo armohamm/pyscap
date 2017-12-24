4112775d1ac714540f8fbf6c14449b6a366ea3b9
Casey Jaymes <cjaymes@gmail.com>
2017-12-24 11:57:48 -0500

    replaced TAG_MAP

--- tests/fixtures/test/__init__.py
+++ tests/fixtures/test/__init__.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with PySCAP.  If not, see <http://www.gnu.org/licenses/>.
 
-TAG_MAP = {
+ELEMENT_MAP = {
     ('http://jaymes.biz/test', 'RootFixture'): 'RootFixture',
     ('http://jaymes.biz/test', 'AttributeFixture'): 'AttributeFixture',
     ('http://jaymes.biz/test', 'RequiredAttributeFixture'): 'RequiredAttributeFixture',
